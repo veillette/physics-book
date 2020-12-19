35d5fdda710cd207a07009248d3b69174f2ecdda
Martin Veillette <martin.veillette@gmail.com>
2020-12-18 21:56:49 -0500

    add support for vector notation (see #5)

--- assets/js/math-config.js
+++ assets/js/math-config.js
@@ -7,8 +7,24 @@ window.MathJax = {
             '[+]': ['ams']
         },
         macros: {
-            RR: "\\mathbf{\\vec{R}}",
-            bold: ["{\\bf #1}", 1]
+            KE: '{\\text{KE}}',                  // kinetic energy
+            PE: '{\\text{PE}}',                  // kinetic energy
+            vb: ['\\mathbf{\\vec{#1}}', 1],     // this macro has one parameter
+            mag: ['\\left|\\mathbf{\\vec{#1}}\\right|', 1],     // this macro has one parameter
+            va: '\\mathbf{\\vec{a}}',     // this macro has one parameter
+            vv: '\\mathbf{\\vec{v}}',     // this macro has one parameter
+            vF: '\\mathbf{\\vec{F}}',     // this macro has one parameter
+            vW: '\\mathbf{\\vec{W}}',     // this macro has one parameter
+            vN: '\\mathbf{\\vec{W}}',     // this macro has one parameter
+            kg: '\\text{kg}',     // this macro has one parameter
+            ms: '\\text{m/s}',     // this macro has one parameter
+            mss: '\\text{m/s}^2',     // this macro has one parameter
+            rs: '\\text{rad/s}',     // this macro has one parameter
+            rss: '\\text{rad/s}^2',     // this macro has one parameter
+            N: '\\text{N}',     // this macro has one parameter
+            J: '\\text{J}',     // this macro has one parameter
+            s: '\\text{s}',     // this macro has one parameter
+            m: '\\text{m}',     // this macro has one parameter
         },
         tags: 'ams'
     }
