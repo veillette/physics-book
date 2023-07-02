d3367022a4f44eee0de1f8d9f71a46e963bfff27
Martin Veillette <martin.veillette@gmail.com>
2023-07-01 20:50:39 -0400

    add service worker (see #60)

---
+++ serviceWorker.js
@@ -0,0 +1,51 @@
+// Perform install steps
+const cacheName = 'textbookCache';
+
+// List the files to precache
+let precacheResources = [
+    '/',
+    'assets/css/styles.css',
+    'assets/css/gitbook.css',
+    'assets/manifest/icons/icon-144x144.png',
+    'assets/js/book-viewer.js',
+    'assets/js/math-config.js'
+];
+
+self.addEventListener('install', function (event) {
+// Perform install steps
+    event.waitUntil(
+        caches.open(cacheName)
+            .then(function (cache) {
+                console.log('Opened cache');
+                return cache.addAll(precacheResources);
+            })
+    );
+});
+
+self.addEventListener('activate', function (event) {
+    const cacheWhitelist = ['random'];
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.map(function (cacheName) {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// When there's an incoming fetch request, try and respond with a precached resource, otherwise fall back to the network
+self.addEventListener('fetch', (event) => {
+    console.log('Fetch intercepted for:', event.request.url);
+    event.respondWith(
+        caches.match(event.request).then((cachedResponse) => {
+            if (cachedResponse) {
+                return cachedResponse;
+            }
+            return fetch(event.request);
+        }),
+    );
+});
