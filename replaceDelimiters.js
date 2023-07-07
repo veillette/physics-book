a9327b9feef3b723e4d6d9eb6df1280636a9cbdf
Martin Veillette <martin.veillette@gmail.com>
2023-07-07 16:28:20 -0400

    add a heuristic latex pattern

--- replaceDelimiters.js
+++ replaceDelimiters.js
@@ -13,6 +13,46 @@ function replaceAllDelimiters(text) {
     return text.replace(pattern, replacement);
 }
 
+function isLikelyLatex(text) {
+    const pattern = /\$\$(.*?)\$\$/gs;
+    const matches = text.match(pattern);
+
+    let isLikely = true;
+    if (matches) {
+        for (const match of matches) {
+            // Remove the $ delimiters from the match
+            const content = match.slice(2, -2);
+
+            // Check if the content contains numbers or simple operations
+
+            // \b\d+(\.\d+)?\b matches a number, either an integer or a decimal.
+            // (\s*[-+*/]\s*\d+(\.\d+)?)* matches zero or more occurrences of a simple operation (addition, subtraction, multiplication, or division) followed by a number.
+            // \s* matches any number of blank space characters before and after the operation.
+            // [-+*/] matches one of the four basic operations: -, +, *, or /.
+            // \d+(\.\d+)? matches a number, either an integer or a decimal, following the operation.
+            // (_|\^) matches either an underscore (_) or a caret (^) character.
+            // \{.*?\} matches the content inside curly braces, representing a subscript or superscript.
+            if (/\b\d+(\.\d+)?(\s*[-+*/=]\s*\d+(\.\d+)?)*(_\^)\{.*?\}\b/.test(content)) {
+                isLikely = isLikely && true; // Likely LaTeX
+            } else// Check if the content contains any common LaTeX commands
+            if (/\b(_\^)\b/.test(content)) {
+                isLikely = isLikely && true; // Likely LaTeX
+            } else
+            if (/\b(alpha|kappa|lambda|epsilon|beta|gamma|delta|theta|omega|tau|pi|nu|mu|rho|sigma|Delta|Theta)\b/.test(content)) {
+                isLikely = isLikely && true; // Likely LaTeX
+            } else if (/\b(prime|circ|left|right|perp|vb|frac|sqrt|array|text|sum|int|cdot|pm|leq|geq|times|rightarrow|tan|sin|cos|log|overline)\b/.test(content)) {
+                isLikely = isLikely && true; // Likely LaTeX
+            } else {
+                isLikely = false; // Not likely LaTeX
+            }
+        }
+
+        return isLikely;
+    } else {
+        return true; // not latex tags
+    }
+}
+
 const folderPath = 'contents';
 
 fs.readdir(folderPath, (err, files) => {
@@ -32,8 +72,10 @@ fs.readdir(folderPath, (err, files) => {
                 }
 
                 let content = data;
-                content = replaceEquationDelimiters(content);
-                content = replaceAllDelimiters(content);
+                const isLatex = isLikelyLatex(content);
+                console.log(`file: ${fileName}, isLatex: ${isLatex}`)
+                // content = replaceEquationDelimiters(content);
+                // content = replaceAllDelimiters(content);
 
                 fs.writeFile(filePath, content, 'utf-8', (err) => {
                     if (err) {
