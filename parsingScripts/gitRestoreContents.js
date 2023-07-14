8f930c860f515218dbbf10634b95e7ea6f3fcc53
Martin Veillette <martin.veillette@gmail.com>
2023-07-14 16:32:20 -0400

    update scripts

--- parsingScripts/gitRestoreContents.js
+++ parsingScripts/gitRestoreContents.js
@@ -1,16 +1,47 @@
 const simpleGit = require('simple-git');
+const fs = require('fs');
 
 // Specify the path to your Git repository
-const repoPath = '';
+const repoPath = '..';
 
 // Create a new simple-git instance
 const git = simpleGit(repoPath);
 
 // Restore all files within the 'contents' directory
-git.checkout('HEAD', ['contents/*'], (error) => {
-    if (error) {
-        console.error('An error occurred while restoring files:', error);
-    } else {
-        console.log('Files restored successfully.');
-    }
-});
+restoreFiles('contents');
+restoreFiles('resources');
+
+function restoreFiles(directory) {
+    git.checkout('HEAD', [`${directory}/*`], (error) => {
+        if (error) {
+            console.error('An error occurred while restoring files:', error);
+        } else {
+            console.log('Files restored successfully.');
+        }
+    });
+
+    // Get the list of Git tracked files
+    git.status((err, status) => {
+        if (err) {
+            console.error('An error occurred while getting Git status:', err);
+            return;
+        }
+
+        // Filter non-tracked files
+        const nonTrackedFiles = status.files
+            .filter(file => file.path() && file.index === '?' && file.working_dir === '?')
+            .map(file => file.path());
+
+        // Delete non-tracked files
+        nonTrackedFiles.forEach(file => {
+            const filePath = `${directory}/${file}`;
+            fs.unlink(filePath, (error) => {
+                if (error) {
+                    console.error(`Failed to delete file ${filePath}:`, error);
+                } else {
+                    console.log(`Deleted file: ${filePath}`);
+                }
+            });
+        });
+    });
+}
