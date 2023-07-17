db546078676b5b0cac207c6acdd4886a0098dd4b
Martin Veillette <martin.veillette@gmail.com>
2023-07-17 18:20:40 -0400

    add convert To WebP scripts for figures

---
+++ parsingScripts/convertToWebp.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const path = require('path');
+const sharp = require('sharp');
+
+const inputDirectory = '../resources'; // Replace with the path to your input directory
+const outputDirectory = '../output'; // Replace with the path to the desired output directory
+
+// Create the output directory if it doesn't exist
+if (!fs.existsSync(outputDirectory)) {
+    fs.mkdirSync(outputDirectory);
+}
+
+fs.readdir(inputDirectory, (err, files) => {
+    if (err) {
+        console.error('Error reading directory:', err);
+        return;
+    }
+
+    files.forEach(file => {
+        const inputPath = path.join(inputDirectory, file);
+        const extension = path.extname(file).toLowerCase();
+        if (extension === '.jpg' || extension === '.jpeg' || extension === '.png') {
+            const fileName = path.basename(file, extension);
+            const outputPath = path.join(outputDirectory, `${fileName}.webp`);
+
+            sharp(inputPath)
+                .toFile(outputPath, (err, info) => {
+                    if (err) {
+                        console.error(`Error converting ${file}:`, err);
+                    } else {
+                        console.log(`Converted ${file} to WebP:`, info);
+                    }
+                });
+        }
+    });
+});
