0dbc5c0c1d2c930d00ccc906fed5070672ba1bea
Martin Veillette <martin.veillette@gmail.com>
2023-07-21 20:02:57 -0400

    prefetch previous and nextpage

--- serviceWorker.js
+++ serviceWorker.js
@@ -12,7 +12,7 @@ let precacheResources = [
 ];
 
 self.addEventListener('install', function (event) {
-// Perform install steps
+    // Perform install steps
     event.waitUntil(
         caches.open(cacheName)
             .then(function (cache) {
@@ -37,7 +37,7 @@ self.addEventListener('activate', function (event) {
     );
 });
 
-// When there's an incoming fetch request, try and respond with a precached resource, otherwise fall back to the network
+// When there's an incoming fetch request, try to respond with a precached resource, otherwise fall back to the network
 self.addEventListener('fetch', (event) => {
     console.log('Fetch intercepted for:', event.request.url);
     event.respondWith(
@@ -45,7 +45,30 @@ self.addEventListener('fetch', (event) => {
             if (cachedResponse) {
                 return cachedResponse;
             }
-            return fetch(event.request);
-        }),
+
+            // Fetch the requested resource from the network
+            const fetchPromise = fetch(event.request).then((response) => {
+                // Cache the fetched resource
+                const clonedResponse = response.clone();
+                caches.open(cacheName).then((cache) => {
+                    cache.put(event.request, clonedResponse);
+                });
+                return response;
+            });
+
+            // Dynamically fetch the URLs for the previous and next modules
+            const prevModuleLink = document.querySelector('.book .navigation-prev');
+            const nextModuleLink = document.querySelector('.book .navigation-next');
+            const prefetchPromises = [];
+            if (prevModuleLink) {
+                prefetchPromises.push(caches.match(prevModuleLink.href));
+            }
+            if (nextModuleLink) {
+                prefetchPromises.push(caches.match(nextModuleLink.href));
+            }
+
+            // Return the first available response (either from cache or network)
+            return Promise.race([fetchPromise, ...prefetchPromises]);
+        })
     );
 });
