7ba95d79154a7718e1c2e668268057d2a65adaba
Martin Veillette <martin.veillette@gmail.com>
2023-07-21 21:11:15 -0400

    improve install

--- serviceWorker.js
+++ serviceWorker.js
@@ -8,7 +8,9 @@ let precacheResources = [
     'assets/css/gitbook.css',
     'assets/manifest/icons/icon-144x144.png',
     'assets/js/book-viewer.js',
-    'assets/js/math-config.js'
+    'assets/js/math-config.js',
+    '/contents/', // Add URLs from directory1
+    '/resources/',
 ];
 
 self.addEventListener('install', function (event) {
@@ -40,49 +42,15 @@ self.addEventListener('activate', function (event) {
     );
 });
 
-// When there's an incoming fetch request, try to respond with a precached resource, otherwise fall back to the network
+// When there's an incoming fetch request, try and respond with a precached resource, otherwise fall back to the network
 self.addEventListener('fetch', (event) => {
-    const requestUrl = new URL(event.request.url);
-
-    // Check if the request is for a module (HTML document)
-    if (requestUrl.pathname.endsWith('.html')) {
-        event.respondWith(
-            caches.match(event.request).then((cachedResponse) => {
-                if (cachedResponse) {
-                    return cachedResponse;
-                }
-
-                // Fetch the requested resource from the network
-                const fetchPromise = fetch(event.request).then((response) => {
-                    // Cache the fetched resource
-                    const clonedResponse = response.clone();
-                    caches.open(cacheName).then((cache) => {
-                        cache.put(event.request, clonedResponse);
-                    });
-                    return response;
-                });
-
-                // Dynamically fetch the URLs for the previous and next modules
-                const prevModuleLink = document.querySelector('.book .navigation-prev');
-                const nextModuleLink = document.querySelector('.book .navigation-next');
-                const prefetchPromises = [];
-                if (prevModuleLink) {
-                    prefetchPromises.push(caches.match(prevModuleLink.href));
-                }
-                if (nextModuleLink) {
-                    prefetchPromises.push(caches.match(nextModuleLink.href));
-                }
-
-                // Return the first available response (either from cache or network)
-                return Promise.race([fetchPromise, ...prefetchPromises]);
-            })
-        );
-    } else {
-        // For non-module requests, use the default cache behavior
-        event.respondWith(
-            caches.match(event.request).then((cachedResponse) => {
-                return cachedResponse || fetch(event.request);
-            })
-        );
-    }
+    console.log('Fetch intercepted for:', event.request.url);
+    event.respondWith(
+        caches.match(event.request).then((cachedResponse) => {
+            if (cachedResponse) {
+                return cachedResponse;
+            }
+            return fetch(event.request);
+        }),
+    );
 });
